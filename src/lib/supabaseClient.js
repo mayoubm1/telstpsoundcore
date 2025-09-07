86745dc384e29bfc16dab159c971cfa974d7a558
3mayoub4-a11y <3m.ayoub4@gmail.com>
2025-09-07 06:08:38 +0300

    feat: connect to Supabase

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
-const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
+const supabaseUrl = 'https://yqldqeacofpmmguxcngm.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlxbGRxZWFjb2ZwbW1ndXhjbmdtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTcyMTA1MTQsImV4cCI6MjA3Mjc4NjUxNH0.cL0XISlQjaHMwLLrGm3LKfNgfV641IwAEevqIO80T54'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
